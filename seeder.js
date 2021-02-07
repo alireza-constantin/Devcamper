b5c623a46649195df680665b428bac4bcca09d69
alireza-constantin <alirezasoheili8080@yahoo.com>
2021-02-07 12:04:50 +0330

    add seeder

---
+++ seeder.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const colors = require('colors');
+
+// load env vars
+dotenv.config({path: './config/config.env'})
+
+// load models
+const Bootcamp = require('./models/Bootcamp')
+
+// connect to db
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+});
+
+// read JSON file
+const bootcamps = JSON.parse(fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8'));
+
+// import data into db
+const importData = async() => {
+    try {
+        await Bootcamp.create(bootcamps);
+        console.log(`Data is imported...`.green.inverse);
+        process.exit()
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+// delete data from db
+const deleteData = async() => {
+    try {
+        await Bootcamp.deleteMany();
+        console.log(`Data is deleted...`.red.inverse);
+        process.exit()
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+if(process.argv[2] === '-i'){
+    importData()
+} else if (process.argv[2] === '-d'){
+    deleteData()
+};
+
